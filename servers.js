97e662dfd4161d30085ae60cdc016bdf9a2f898c
Dioniii <90280629+Dioniii@users.noreply.github.com>
2025-03-30 23:53:41 +0200

    connected database - TABLE USERS

--- servers.js
+++ servers.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
-const{sql, poolPormise} = require('./db.js');
+const{sql, poolPormise, poolPromise} = require('./db.js');
 const{pool} = require('mssql');
 
 const app = express();
@@ -13,4 +13,26 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`SERVER IS RUNNING ON ${PORT}`));
 
 
-console.log("nice");
+app.get("/users" , async(req,res) => {
+    try{
+        const pool = await poolPromise;
+        const result = await pool.request().query(
+            "Select * From users"
+        )
+
+        console.log(result);
+
+        res.status(200).json({
+            success:true,
+            uData: result.recordset
+        })
+    }catch(error){
+        console.log(error);
+
+        res.status(404).json({
+            success:false,
+            message: "Errror",
+            error: error.message
+        })
+    }
+})
